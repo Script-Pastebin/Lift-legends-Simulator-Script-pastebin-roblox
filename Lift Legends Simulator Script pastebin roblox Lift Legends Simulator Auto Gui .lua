d82406f99a7ff9ef4303eb829ee1725667d31cc9
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 06:59:47 +0100

    commit zero

---
+++ Lift Legends Simulator Script pastebin roblox Lift Legends Simulator Auto Gui .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/s7r5SMHQ", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							coolLift Legends Simulator auto-gui. It has Auto-lift, Auto-rebirth and more!
+						
+]]
